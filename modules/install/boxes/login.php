9226d847f3d6033d7ebd4c8765d8872edfef93d2
woikerl <alexander.woike@gmail.com>
2017-04-14 15:01:28 +0200

    restructure login box; import default ls styles, clean up base css

--- modules/install/boxes/login.php
+++ modules/install/boxes/login.php
@@ -6,16 +6,19 @@
  * @author knox
  * @version $Id: login.php 1798 2009-01-18 17:46:03Z maztah $
  */
+$smarty->assign('l_login', 'Einloggen');
+$smarty->assign('l_register', t('Registrieren'));
+$smarty->assign('l_pwrecover', t('Passwort vergessen'));
 
-if ($_GET['mod'] == 'logout') $smarty->assign('action', 'index.php');
-else $smarty->assign('action', 'index.php?mod=auth&action=login');
-$smarty->assign('buttons_add', $dsp->FetchIcon('index.php?mod=signon', 'add_user', t('Registrieren')));
-$smarty->assign('buttons_pw', $dsp->FetchIcon('index.php?mod=usrmgr&amp;action=pwrecover', 'pw_forgot',t('Passwort vergessen')));
-$smarty->assign('buttons_login', '<input type="submit" class="Button" name="login" value="Einloggen" />');
+$smarty->assign('u_login', 'index.php' . ($_GET['mod'] == 'logout' ? '' : '?mod=auth&action=login'));
+$smarty->assign('u_register', 'index.php?mod=signon');
+$smarty->assign('u_pwrecover', 'index.php?mod=usrmgr&amp;action=pwrecover');
 
+/*
 // 62.67.200.4 = Proxy IP of https://sslsites.de/lansuite.orgapage.de
 if ($cfg['sys_partyurl_ssl'] and ($_SERVER['HTTPS'] != 'on' and getenv(REMOTE_ADDR) != "62.67.200.4"))
-  $smarty->assign('ssl_link', $cfg['sys_partyurl_ssl']);
+  $smarty->assign('l_ssl_link', 'SSL Login');
+  $smarty->assign('u_ssl_link', $cfg['sys_partyurl_ssl']); // hide ssl-link::woikerl*/
 
 $box->AddTemplate($smarty->fetch('modules/boxes/templates/box_login_content.htm'));
 ?>
